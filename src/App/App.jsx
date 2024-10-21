d28588f045d07be2a1c49a1ad08f78e6386d092f
Jintae Song <w.jinttao@gmail.com>
2024-10-21 16:42:17 +0900

    feat(main, App) React Router 기본 설정

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Header from '../Header'
+import Home from '../Home'
 import '../App.css'
 
 function App() {
   return (
     <>
+    <div>App</div>
+    <Header />
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+    </Routes>
     </>
   )
 }
